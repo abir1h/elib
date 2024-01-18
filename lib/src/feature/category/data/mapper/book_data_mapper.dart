df25c2834d498ee498b0d5a12b84a56b89bb69de
abir <abirrahman959@gmail.com>
2024-01-18 09:44:18 +0600

    added data models antitny & mapper v4

---
+++ lib/src/feature/category/data/mapper/book_data_mapper.dart
@@ -0,0 +1,68 @@
+import 'package:elibrary/src/feature/category/data/models/book_data_model.dart';
+import 'package:elibrary/src/feature/category/domain/entities/book_data_entity.dart';
+
+abstract class BookDataMapper<M, E> {
+  M fromEntityToModel(E entity);
+  E toEntityFromModel(M model);
+}
+
+class _BookDataModelToEntityMapper
+    extends BookDataMapper<BookDataModel, BookDataEntity> {
+  @override
+  BookDataModel fromEntityToModel(BookDataEntity entity) {
+    return BookDataModel(
+        id: entity.id,
+        adminId: entity.adminId,
+        categoryId: entity.categoryId,
+        authorId: entity.authorId,
+        titleEn: entity.titleEn,
+        titleBn: entity.titleBn,
+        slug: entity.slug,
+        descriptionEn:entity. descriptionEn,
+        descriptionBn:entity. descriptionBn,
+        coverImage:entity. coverImage,
+        bookFile: entity.bookFile,
+        externalLink:entity.externalLink,
+        totalPages: entity.totalPages,
+        totalChapters: entity.totalChapters,
+        isGenerateBook: entity.isGenerateBook,
+        status: entity.status,
+        createdAt: entity.createdAt,
+        updatedAt: entity.updatedAt,
+        deletedAt:entity. deletedAt);
+  }
+
+  @override
+  BookDataEntity toEntityFromModel(BookDataModel model) {
+    return BookDataEntity(
+        id: model.id,
+        adminId:model. adminId,
+        categoryId: model.categoryId,
+        authorId: model.authorId,
+        titleEn: model.titleEn,
+        titleBn: model.titleBn,
+        slug: model.slug,
+        descriptionEn: model.descriptionEn,
+        descriptionBn: model.descriptionBn,
+        coverImage:model.coverImage,
+        bookFile: model.bookFile,
+        externalLink:model. externalLink,
+        totalPages: model.totalPages,
+        totalChapters: model.totalChapters,
+        isGenerateBook:model. isGenerateBook,
+        status: model.status,
+        createdAt:model. createdAt,
+        updatedAt:model. updatedAt,
+        deletedAt:model. deletedAt);
+  }
+}
+
+extension BookDataModelExt on BookDataModel {
+  BookDataEntity get toBookDataEntity =>
+      _BookDataModelToEntityMapper().toEntityFromModel(this);
+}
+
+extension BookDataEntityExt on BookDataEntity {
+  BookDataModel get toAuthDataModel =>
+      _BookDataModelToEntityMapper().fromEntityToModel(this);
+}
