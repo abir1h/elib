076d5367345daf495dfd25125d152f217a0a9540
abir <abirrahman959@gmail.com>
2024-01-25 13:07:04 +0600

    added Note data model,entity,mapper v2

--- lib/src/feature/note/data/models/note_data_model.dart
+++ lib/src/feature/note/data/models/note_data_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 import '../../../book/data/models/book_data_model.dart';
+
 @immutable
 class NoteDataModel {
   final int id;
@@ -24,26 +25,30 @@ class NoteDataModel {
   });
 
   factory NoteDataModel.fromJson(Map<String, dynamic> json) => NoteDataModel(
-    id: json["id"]??-1,
-    bookId: json["book_id"]??-1,
-    emisUserId: json["emis_user_id"]??-21,
-    note: json["note"]??"",
-    createdAt: json["created_at"]??"",
-    updatedAt: json["updated_at"]??"",
-    deletedAt: json["deleted_at"]??"",
-    book:  json['book'] != null
-        ? BookDataModel.fromJson(json['book'])
-        : null,
-  );
+        id: json["id"] ?? -1,
+        bookId: json["book_id"] ?? -1,
+        emisUserId: json["emis_user_id"] ?? -21,
+        note: json["note"] ?? "",
+        createdAt: json["created_at"] ?? "",
+        updatedAt: json["updated_at"] ?? "",
+        deletedAt: json["deleted_at"] ?? "",
+        book:
+            json['book'] != null ? BookDataModel.fromJson(json['book']) : null,
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "book_id": bookId,
-    "emis_user_id": emisUserId,
-    "note": note,
-    "created_at": createdAt,
-    "updated_at": updatedAt,
-    "deleted_at": deletedAt,
-  };
-
+        "id": id,
+        "book_id": bookId,
+        "emis_user_id": emisUserId,
+        "note": note,
+        "created_at": createdAt,
+        "updated_at": updatedAt,
+        "deleted_at": deletedAt,
+      };
+  static List<NoteDataModel> listFromJson(List<dynamic> json) {
+    return json.isNotEmpty
+        ? List.castFrom<dynamic, NoteDataModel>(
+            json.map((x) => NoteDataModel.fromJson(x)).toList())
+        : [];
+  }
 }
